8f71c292841990ecedacf62be4689bb793f74210
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-11-14 16:51:26 +0100

    change default config

--- src/contexts/ConfigContext.js
+++ src/contexts/ConfigContext.js
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 
 export const configStartingValues = {
-  difficulty: "easy",
-  numberOfQuestions: 10,
+  difficulty: "medium",
+  numberOfQuestions: 20,
 }
 
 export default createContext(configStartingValues);
