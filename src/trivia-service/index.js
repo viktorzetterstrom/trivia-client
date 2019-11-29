22dc5b7901c276c45e708e4e9f702574a9f4ff2c
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-11-29 14:19:23 +0100

    fix api urls

--- src/trivia-service/index.js
+++ src/trivia-service/index.js
@@ -1,8 +1,8 @@
 const qs = require('querystring');
 
 const apiUrl = process.env.NODE_ENV === 'development'
-? 'http://localhost:4000'
-: 'https://api.zetterstrom.dev/trivia';
+  ? 'https://localhost:4443/trivia'
+  : 'https://api.zetterstrom.dev/trivia';
 
 const triviaService = {
   questions: (config) => fetch(`${apiUrl}/questions/?${qs.encode(config)}`).then(res => res.json())
