7310b2b9383c17131c067272ed37f378475964f3
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-11-29 11:48:57 +0100

    change api-url with new rev-proxy

--- src/trivia-service/index.js
+++ src/trivia-service/index.js
@@ -1,8 +1,8 @@
 const qs = require('querystring');
 
 const apiUrl = process.env.NODE_ENV === 'development'
-? 'http://localhost:3001'
-: 'https://api.zetterstrom.dev/';
+? 'http://localhost:4000'
+: 'https://api.zetterstrom.dev/trivia';
 
 const triviaService = {
   questions: (config) => fetch(`${apiUrl}/questions/?${qs.encode(config)}`).then(res => res.json())
